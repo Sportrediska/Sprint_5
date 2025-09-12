1e173e55baf46377a8508d85a80cf1b604cff96a
Sportrediska <anatolyskorov@yandex.ru>
2025-09-13 00:44:29 +0300

    Финальные правки_1

--- tests/test_user_login.py
+++ tests/test_user_login.py
@@ -1,7 +1,6 @@
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
-from ..locators import AdLocators
 from ..locators import UserLoginLocators
 from ..locators import UserRegistrationLocators
 
